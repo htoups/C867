0d4875b29ceface1084cf0775556673d36c5c4a8
Shawn Warnock <swarnock89@hotmail.com>
2019-03-12 21:26:35 -0500

    Fixed pointer errors

--- roster.h
+++ roster.h
@@ -2,16 +2,20 @@
 #define ROSTER 
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Roster {
 private:
-	const string studentData[5] = { "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", 
-		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,Hannah,Toups,htoups@wgu.edu 24, 32, 39, 43, SOFTWARE" };
+	string firstStudent = "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY";
+	string secondStudent = "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK";
+	string thirdStudent = "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE";
+	string fourthStudent = "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY";
+	string fifthStudent = "A5,Hannah,Toups,htoups@wgu.edu 24, 32, 39, 43, SOFTWARE";
+
+	const vector<string> studentData = { firstStudent, secondStudent, thirdStudent, fourthStudent, fifthStudent };
+
 
 public:
 	string GetStudentData();
